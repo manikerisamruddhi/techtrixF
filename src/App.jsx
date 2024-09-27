13a6a294dbd5d18ea6555c0376122e05d7e8887f
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-27 18:28:15 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TicketList from './pages/Tickets/Tickets';
+import CreateTicket from './components/Ticket/CreateTicket';
+import Quotations from './pages/Quotations/QuotationList';
 // import InvoiceList from './pages/Invoices/InvoiceList';
 // import InvoiceDetails from './pages/Invoices/InvoiceDetails';
 // import CreateInvoice from './pages/Invoices/CreateInvoice';
@@ -8,10 +11,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/AdminDashboard';
 // import Users from './pages/Users/Users';
 // import Customers from './pages/Customers/Customers';
-// import Header from './components/Header';
+import Header from './components/Header/Header';
 import Login from './pages/auth/LoginPage';
 import ForgotPass from './pages/auth/ForgotPasswordPage'
 import Register from './pages/auth/RegisterPage';
+import AdminHomeDash from './pages/dashboard/AdminDashHome'
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from './theme'; // Custom theme
@@ -22,14 +26,22 @@ const App = () => {
             
             <CssBaseline />
             <Router>
+                <Header/>
                 <Routes>
                     {/* Public Routes */}
                     <Route path="/login" element={<Login />} />
                     <Route path="/forgot-password" element={<ForgotPass />} />
                     <Route path="/register" element={<Register />} />
-                    
+                    <Route path="/TicketList" element={<TicketList />} />
+               
                     
                     {/* Unprotected Routes */}
+
+                    <Route path="/CreateTicket" element={<CreateTicket />} />
+                    <Route path="/Quotations" element={<Quotations />} />
+                    
+
+
                     {/* <Route path="/invoices" element={<InvoiceList />} />
                     <Route path="/invoice/:invoiceId" element={<InvoiceDetails />} />
                     <Route path="/create-invoice" element={<CreateInvoice />} />
@@ -41,6 +53,8 @@ const App = () => {
 
                     {/* Default Route */}
                     <Route path="/" element={<Dashboard />} />
+                 
+
                 </Routes>
             </Router>
         </ThemeProvider>
