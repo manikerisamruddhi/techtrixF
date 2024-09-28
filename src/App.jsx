ff8203ba398865dfe0542ff93ebb37d4d9b255aa
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-09-28 17:28:27 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import TicketList from './pages/Tickets/Tickets';
 import CreateTicket from './components/Ticket/CreateTicket';
 import Quotations from './pages/Quotations/QuotationList';
 import Dashboard from './pages/dashboard/AdminDashHome';
+import UserManagement from './pages/dashboard/UserManagement'
 import Login from './pages/auth/LoginPage';
 import ForgotPass from './pages/auth/ForgotPasswordPage';
 import Register from './pages/auth/RegisterPage';
+import Customers from './pages/Customers/Customers';
+import Invoices from './pages/Invoice';
 import { ThemeProvider } from '@mui/material/styles'; // You can choose to keep MUI or remove it if you're fully switching to Ant Design
 import { CssBaseline } from '@mui/material'; // Same as above
 import theme from './theme'; // Custom theme
@@ -26,9 +29,13 @@ const App = () => {
                         <Route path="/register" element={<Register />} />
                         <Route path="/TicketList" element={<TicketList />} />
                         
-                        {/* Unprotected Routes */}
+                        {/* protected Routes */}
                         <Route path="/CreateTicket" element={<CreateTicket />} />
                         <Route path="/Quotations" element={<Quotations />} />
+                        <Route path="/UserManagement" element={<UserManagement />} />
+                        <Route path="/Customers" element={<Customers />} />
+                        <Route path="/Invoices" element={<Invoices />} />
+
 
                         {/* Default Route */}
                         <Route path="/" element={<Dashboard />} />
