495be64826f31a2ab4bdf777bb68b663a38b7c24
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-10-05 16:25:01 +0530

    space is given

--- src/pages/Tickets/TicketDetailsModal.jsx
+++ src/pages/Tickets/TicketDetailsModal.jsx
@@ -14,16 +14,16 @@ const TicketDetailsModal = ({ visible, ticket, onClose }) => {
             {ticket && (
                 <div>
                     <p><strong>Ticket ID:</strong> {ticket.id}</p>
-                    <p><strong>Customer ID:</strong> {ticket.CustomerID}</p>
-                    <p><strong>Title:</strong> {ticket.Title}</p>
-                    <p><strong>Created By:</strong> {ticket.CreatedBy}</p>
-                    <p><strong>Priority:</strong> {ticket.Priority}</p>
-                    <p><strong>Remark:</strong> {ticket.Remark}</p>
-                    <p><strong>Status:</strong> {ticket.Status}</p>
-                    {/* <p><strong>Category:</strong> {ticket.Category}</p> */}
-                    <p><strong>Created Date:</strong> {new Date(ticket.CreatedDate).toLocaleString()}</p>
-                    <p><strong>Resolved:</strong> {ticket.IsResolved ? 'Yes' : 'No'}</p>
-                    <p><strong>Assigned To ID:</strong> {ticket.AssignedToID}</p>
+                    <p><strong>Title :</strong> {ticket.Title}</p>
+                    <p><strong>Customer ID :</strong> {ticket.CustomerID}</p>
+                    <p><strong>Created By :</strong> {ticket.CreatedBy}</p>
+                    <p><strong>Priority :</strong> {ticket.Priority}</p>
+                    <p><strong>Remark :</strong> {ticket.Remark}</p>
+                    <p><strong>Status :</strong> {ticket.Status}</p>
+                    {/* <p><strong>Category :</strong> {ticket.Category}</p> */}
+                    <p><strong>Created Date :</strong> {new Date(ticket.CreatedDate).toLocaleString()}</p>
+                    <p><strong>Resolved :</strong> {ticket.IsResolved ? 'Yes' : 'No'}</p>
+                    <p><strong>Assigned To ID :</strong> {ticket.AssignedToID}</p>
                 </div>
             )}
         </Modal>
