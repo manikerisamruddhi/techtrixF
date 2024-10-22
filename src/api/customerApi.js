b2a639c21ea80157e527504ffdd02f916d2060ab
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-10-22 11:04:55 +0530

    canging

--- src/api/customerApi.js
+++ src/api/customerApi.js
@@ -2,20 +2,22 @@ import axiosInstance from './axiosInstance';
 
 const customerApi = {
     getAllCustomers: () => {
-        return axiosInstance.get('/customers');
+        return axiosInstance.get('/customers/all');
     },
     getCustomerById: (id) => {
-        return axiosInstance.get(`/customers/${id}`);
+        return axiosInstance.get(`/customers/get/${id}`);
     },
     createCustomer: (customerData) => {
-        return axiosInstance.post('/customers', customerData);
+        return axiosInstance.post('/customers/add', customerData);
     },
     updateCustomer: (id, customerData) => {
-        return axiosInstance.put(`/customers/${id}`, customerData);
+        return axiosInstance.put(`/customers/update/${id}`, customerData);
     },
     deleteCustomer: (id) => {
         return axiosInstance.delete(`/customers/${id}`);
     },
 };
+
+// {{base-url}}/api/customers/pagination?page=1&size=1&sort=firstName,asc
 
 export default customerApi;
