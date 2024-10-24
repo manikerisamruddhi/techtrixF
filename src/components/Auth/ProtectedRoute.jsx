e90b4f9c1366a92e2b067545144e86df6abef09a
Ashish Bhosale <bhosaleashish311@gmail.com>
2024-10-24 14:59:57 +0530

    deleted the role filter

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -1,26 +1,15 @@
 // src/components/Auth/ProtectedRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { message } from 'antd';
 
-const ProtectedRoute = ({ children, allowedRoles }) => {
-    const { isAuthenticated, user } = useSelector((state) => state.users);
-
-   //console.log("User:", user);
-//console.log("Is Authenticated:", isAuthenticated);
-
-
+const ProtectedRoute = ({ children }) => {
+    const user = JSON.parse(localStorage.getItem('user')); // Get user from local storage
+   
     // Check if the user is authenticated
-    if (!isAuthenticated) {
+    if (!user) {
         message.warning("You need to be logged in to view this page. Please log in first.");
         return <Navigate to="/login" replace />;
-
-    }
-
-    // Check if the user has the required role
-    if (allowedRoles && !allowedRoles.includes(user?.role)) {
-        return <Navigate to="/login" replace />; // You can redirect to an unauthorized page or home
     }
 
     return children; // Render the child components (protected content)
